a21bb1244b4219a9be80d5cec917e3783a305d27
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2017-09-01 09:01:17 -0400

    Update GRLdoiExtractor.R

--- GRLdoiExtractor.R
+++ GRLdoiExtractor.R
@@ -1,8 +1,19 @@
 # Crossref API documents: https://github.com/CrossRef/rest-api-doc
 # rOpenSci rcrossref tutorial: https://ropensci.org/tutorials/rcrossref_tutorial.html
 
-library("rcrossref")
+library(rcrossref)
+library(rAltmetric)
+library(plyr)
+
 #get all the dois from CrossRef for GRL, which has a print ISSN of 0094-8276
 GRLdata <-cr_works(filter = c(issn = "0094-8276"),limit = 1000,cursor='*',cursor_max = 36939)
-
-#export a list of dois
+#pull out the dataframe
+GRLDF<-GRLdata$data
+#export a list of dois.
+doi_list <- paste0("doi","/", GRLDF$DOI)
+#then run through rAltmetric (code I have already)
+raw_metrics <- llply(doi_list, altmetrics, .progress = 'text')
+metric_data <- ldply(raw_metrics, altmetric_data)
+merged_data <-merge(x =GRLDF , y = metric_data, by.y = "doi", by.x = "DOI", all.x = TRUE)
+write.csv(merged_data, file = "/MERGEDDATA.csv")
+#....

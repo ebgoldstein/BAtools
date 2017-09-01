5300c33922b9cbec6840b8e69108ce5e66bd1bcd
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2017-09-01 09:59:29 -0400

    xtra save step

--- GRLdoiExtractor.R
+++ GRLdoiExtractor.R
@@ -7,13 +7,14 @@ library(plyr)
 
 #get all the dois from CrossRef for GRL, which has a print ISSN of 0094-8276
 GRLdata <-cr_works(filter = c(issn = "0094-8276"),limit = 1000,cursor='*',cursor_max = 36939)
-#pull out the dataframe
+#pull out the dataframe and save it
 GRLDF<-GRLdata$data
+write.csv(GRLDF, file = "GRLDOIDATA.csv")
 #export a list of dois.
 doi_list <- paste0("doi","/", GRLDF$DOI)
 #then run through rAltmetric (code I have already)
 raw_metrics <- llply(doi_list, altmetrics, .progress = 'text')
 metric_data <- ldply(raw_metrics, altmetric_data)
 merged_data <-merge(x =GRLDF , y = metric_data, by.y = "doi", by.x = "DOI", all.x = TRUE)
-write.csv(merged_data, file = "/MERGEDDATA.csv")
+write.csv(merged_data, file = "MERGEDDATA.csv")
 #....

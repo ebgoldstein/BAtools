ac07417ebc25b4f53ca3e938345136b576d23128
Evan B. Goldstein <ebgoldstein@users.noreply.github.com>
2017-12-15 20:18:59 -0500

    looping now

--- WBlame.R
+++ WBlame.R
@@ -1,34 +1,53 @@
 library("tidyverse")
 library("rvest")
 
+#make some new rows to hold the new data
+Wikipapers["WB_Authors"] <- NA
+Wikipapers["WB_edit_times"] <- NA
+Wikipapers["WB_ver_history"] <- NA
 
-article <- "enceladus"
-needle <- "showman"
-#needle can;t be DOI neccesarily, because some poeple ignore doi... so it needs to be author name?)
-
-
-beginning <- "http://wikipedia.ramselehof.de/wikiblame.php?user_lang=en&lang=en&project=wikipedia&article=" 
-after <- "&skipversions=0&ignorefirst=0&limit=500&offmon=10&offtag=16&offjahr=2017&searchmethod=int&order=asc&user="
-
-site <-  paste0(beginning, article, "&needle=", needle, after)
-
-wikiblame <- read_html(site)
+#Notes:
+# -some people might not write in doi
+# -preprints and alternative titles also confuse matters
+#loop through the papers to populate the dataframe with Wikipedia edit details
+for (k in 504:nrow(Wikipapers)) {
+  #Wikipedia page to search
+  article <- Wikipapers[k, "W_Pages"]
+  #replace spaces with '+' sign
+  article <- gsub(" ", "+", article, fixed = TRUE)
+  #DOI to find on the page
+  needle <- Wikipapers[k, "DOI"]
   
-#scrape for edit
-spedit <- wikiblame %>%
-  html_nodes("br~ a+ a") %>%
-  html_attr('href')
-
-
-# now follow the link to find the editor who added the 'needle': 
-
-wiki <- read_html(spedit[2])
-
-editor <- wiki %>%
-  html_nodes("#mw-diff-ntitle2 bdi") %>%
-  html_text()
-
-# and determine the data that the editor added the needle
-editdate <- wiki %>%
-  html_nodes("#mw-diff-ntitle1 strong > a") %>%
-  html_text()
+  beginning <-
+    "http://wikipedia.ramselehof.de/wikiblame.php?user_lang=en&lang=en&project=wikipedia&article="
+  after <-
+    "&skipversions=0&ignorefirst=0&limit=500&offmon=10&offtag=16&offjahr=2017&searchmethod=int&order=desc&force_wikitags=on&user="
+  
+  site <-  paste0(beginning, article, "&needle=", needle, after)
+  
+  wikiblame <- read_html(site)
+  
+  #find edit with Wikiblame
+  spedit <- wikiblame %>%
+    html_nodes("br~ a+ a") %>%
+    html_attr('href')
+  
+  #this catches where Wikiblame has trouble including 1) when edits are deleted 2) when the page is a redirect 3) other problems???
+  if (length(spedit) > 1) {
+    # now follow the link to find the editor who added the 'needle':
+    lastlink<-tail(spedit,n=1)
+    Wikipapers[k, "WB_ver_history"] <-(lastlink)
+    wiki <- read_html(lastlink)
+    
+    #scrape for editor name and add to DF
+    Wikipapers[k, "WB_Authors"] <- wiki %>%
+      html_nodes("#mw-diff-ntitle2 bdi") %>%
+      html_text()
+    
+    # scrape for date and add to DF
+    Wikipapers[k, "WB_edit_times"] <- wiki %>%
+      html_nodes("#mw-diff-ntitle1 strong > a") %>%
+      html_text()
+  }
+  print(k) #for debugging
+}
